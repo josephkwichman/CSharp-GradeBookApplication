56bc6f4af7d7ab3df85e346dcdf91823a3b80883
Joseph Wichman <josephkwichman@gmail.com>
2018-03-11 11:26:10 -0500

    Override Statistics
    1. Create CalculateStatistics Override
    2. Create CalculateStudentStatistics Override

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -6,14 +6,14 @@ using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
-    public class RankedGradeBook: BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
         }
 
-         public override char GetLetterGrade(double averageGrade)
+        public override char GetLetterGrade(double averageGrade)
         {
             if (Students?.Count < 5)
             {
@@ -27,7 +27,7 @@ namespace GradeBook.GradeBooks
             {
                 return 'A';
             }
-            else if(grades[(threshold * 2) - 1] <= averageGrade)
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
             {
                 return 'B';
             }
@@ -40,6 +40,26 @@ namespace GradeBook.GradeBooks
                 return 'D';
             }
             return 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students?.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students?.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
         }
     }
 }
